9fa3420abfc7bda7118941049ffe62cb1de7f9e7
Nguyễn Thanh Trung <thanhtruongba2001@gmail.com>
2023-10-14 21:49:51 +0700

    done image

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -40,8 +40,7 @@ const Modal = ({ isOpen, onClose, data }: any) => {
     const file = e.target.files[0]
     if (file) {
       formData.append('avatar', file)
-      console.log(formData)
-      mutationChangeAvatar.mutate(formData, {
+      mutationChangeAvatar.mutate(file, {
         onSuccess: () => {
           toast.success('Đổi avatar thành công!')
         },
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -11,7 +11,7 @@ class Http {
       baseURL: 'https://api-kinhdoanh.onrender.com/api/',
       timeout: 10000,
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'multipart/form-data', 
       }
     })
     this.instance.interceptors.request.use(
